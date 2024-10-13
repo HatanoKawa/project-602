69625a1a95d86c2bafea9088a21a49b6457208cb
hatanokawa <hatanokawa@qq.com>
2024-10-14 03:13:49 +0800

    fix: 切换到hash路由以解决github pages无法识别history路由的问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import IntroductionPage from "@/views/introduction/IntroductionPage.vue";
 import GamePage from "@/views/GamePage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
