af74ff34c22dd0a6d1b67b30f411f53996eb3db2
hatanokawa <hatanokawa@qq.com>
2024-10-13 19:36:45 +0800

    chore: 构建配置调整

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import Markdown from 'unplugin-vue-markdown/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project-602/',
   plugins: [
     vue({include: [/\.vue$/, /\.md$/],}),
     vueJsx(),
